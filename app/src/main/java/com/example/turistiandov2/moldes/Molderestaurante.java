61397dcbdb82c3ba24dac06f346cabdee28e1d54
r3l4x1n <nfzapataca@cesde.net>
2023-08-08 20:57:41 -0500

    listas

---
+++ app/src/main/java/com/example/turistiandov2/moldes/Molderestaurante.java
@@ -0,0 +1,62 @@
+package com.example.turistiandov2.moldes;
+
+public class Molderestaurante {
+
+    private String nombre;
+    private String telefono;
+    private String rangoprecio;
+    private String platorecomendado;
+    private  Integer foto;
+
+    public Molderestaurante() {
+
+    }
+
+    public Molderestaurante(String nombre, String telefono, String rangoprecio, String platorecomendado, Integer foto) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.rangoprecio = rangoprecio;
+        this.platorecomendado = platorecomendado;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getRangoprecio() {
+        return rangoprecio;
+    }
+
+    public void setRangoprecio(String rangoprecio) {
+        this.rangoprecio = rangoprecio;
+    }
+
+    public String getPlatorecomendado() {
+        return platorecomendado;
+    }
+
+    public void setPlatorecomendado(String platorecomendado) {
+        this.platorecomendado = platorecomendado;
+    }
+
+    public Integer getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Integer foto) {
+        this.foto = foto;
+    }
+}
