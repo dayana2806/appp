1ea58efbd4504a639a17e3d603536b2cef0621c6
ItsAristi02 <alejandraaristizabal49@gmail.com>
2023-09-20 20:50:57 -0500

    base de datos-incompleto

--- app/src/main/java/com/example/turistiandov2/Listahoteles.java
+++ app/src/main/java/com/example/turistiandov2/Listahoteles.java
@@ -1,14 +1,23 @@
 package com.example.turistiandov2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 
 import com.example.turistiandov2.adaptadores.AdaptadoHoteles;
 import com.example.turistiandov2.moldes.Moldehotel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -18,12 +27,33 @@ public class Listahoteles extends AppCompatActivity {
     ArrayList<Moldehotel> listaHoteles  = new ArrayList<>();
     RecyclerView recyclerView;
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listahoteles);
         recyclerView=findViewById(R.id.listadinamicahoteles);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+
+        db.collection("hoteles")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull  Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                               String nombreHotel=document.getString("nombre");
+                               String precioHotel=document.getString("precio");
+                                Toast.makeText(Listahoteles.this, nombreHotel, Toast.LENGTH_SHORT).show();
+
+                            }
+                        } else {
+                           // Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
         llenarListaConDatos();
         AdaptadoHoteles adaptadoHoteles=new AdaptadoHoteles(listaHoteles);
